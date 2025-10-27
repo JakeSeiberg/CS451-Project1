10c43d51a8c27ce3c3a7a359c10a938d35b6bfb2
benster1024 <benster1024@gmail.com>
2025-10-26 18:47:52 -0700

    page and table logic changes

--- lstore/table.py
+++ lstore/table.py
@@ -27,12 +27,20 @@ class Table:
         self.num_columns = num_columns
         self.page_directory = {}
         self.index = Index(self)
-        self.base_page = []
+        self.base_page = [[Page()] for _ in range(num_columns)]
         self.tail_pages = []
         self.rid_counter = 1
         pass
     def insert_row(self, columns):
         rid = self.table[columns]
+        for i, value in enumerate(columns):
+            current_page = self.base_page[i][-1]
+            if not current_page.has_capacity:
+                new_page = Page()
+                self.base_page[i].append(new_page)
+                current_page = new_page
+            current_page.write(column)
+        
         self.rid_counter += 1
         
 
