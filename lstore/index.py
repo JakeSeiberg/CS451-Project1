5c1fee806b8417e3c6b3ea933274cafdfc4f3fd1
Jacob Seiberg <jakeseiberg@gmail.com>
2025-10-27 21:11:06 -0700

    b plus tree implementation

--- lstore/index.py
+++ lstore/index.py
@@ -1,12 +1,12 @@
-"""
-A data strucutre holding indices for various columns of a table. Key column should be indexd by default, other columns can be indexed through this object. Indices are usually B-Trees, but other data structures can be used as well.
-"""
+
+import math
 
 class Index:
 
     def __init__(self, table):
         # One index for each table. All our empty initially.
         self.indices = [None] *  table.num_columns
+
         pass
 
     """
@@ -36,3 +36,99 @@ class Index:
 
     def drop_index(self, column_number):
         pass
+
+class BPlusTree:
+    def __init__(self, order=4):
+        self.root = Node(order)
+        self.order = order
+
+    def insert(self, key, rid):
+        root = self.root
+        
+        if (len(root.keys) == self.order - 1):
+            new_root = Node(self.order)
+            new_root.is_leaf = False
+            new_root.children.append(self.root)
+            self.split_child(new_root, 0)
+            self.root = new_root
+
+        self.insert_non_full(self.root, key, rid)
+
+    def insert_non_full(self, node, key, rid):
+        if (node.is_leaf):
+            idx = 0
+
+            while (idx < len(node.keys) and node.keys[idx][0] < key):
+                idx += 1
+            
+            node.keys.insert(idx, (key,rid))
+        else:
+            i = 0
+            while (i < len(node.keys) and key >= node.keys[i]):
+                i += 1
+            
+            if (i < len(node.children[i].keys) and key >= node.keys[i]):
+                self._split_child(node, i)
+                if key >= node.keys[i]:
+                    i += 1
+
+            self._insert_non_full(node.children[i], key, rid)
+        
+    def _split_child(self, parent, index):
+        node = parent.children[index]
+        mid = self.order // 2
+        new_node = Node(self.order)
+        new_node.is_leaf = node.is_leaf
+
+        parent.keys.insert(index, node.keys[mid][0])
+        new_node.keys = node.keys[mid:] if node.is_leaf else node.keys[mid + 1:]
+        node.keys = node.keys[:mid]
+
+        if (not node.is_leaf):
+            new_node.children = node.children[mid + 1:]
+            node.children = node.children[:mid + 1]
+        else:
+            new_node.next = node.next
+            node.next = new_node
+
+        parent.children.insert(index + 1, new_node)
+        
+    def locate(self, key):
+        node = self.root
+        while not node.is_leaf:
+            i = 0
+            while i < len(node.keys) and key >= node.keys[i]:
+                i += 1
+            node = node.children[i]
+
+        return [rid for k, rid in node.keys if k == key]
+    
+    def locate_range(self, start, end):
+        node = self.root
+        while not node.is_leaf:
+            i = 0
+            while i < len(node.keys) and start >= node.keys[i]:
+                i += 1
+            node = node.children[i]
+
+        results = []
+        while node:
+            for k, rid in node.keys:
+                if start <= k <= end:
+                    results.append(rid)
+                elif k > end:
+                    return results
+            node = node.next
+        return results
+
+
+
+class Node:
+
+    def __init__(self, order):
+        self.order = order
+        self.keys = []
+        self.children = []
+        self.is_leaf = True
+        self.next = None
+        
